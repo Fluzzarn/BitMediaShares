1682f732f9957d1abdafc30d6373bca719daef3c
Unknown <fluzzarn@gmail.com>
2018-02-19 17:48:32 -0500

    Basic UI layout

--- Bits4MediaShareBot/Form1.Designer.cs
+++ Bits4MediaShareBot/Form1.Designer.cs
@@ -28,13 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabBar = new System.Windows.Forms.TabControl();
+            this.optionsTab = new System.Windows.Forms.TabPage();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.channelLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bitMinimumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bpsUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.viewMinimumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabBar.SuspendLayout();
+            this.optionsTab.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bitMinimumUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bpsUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewMinimumUpDown)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabBar
+            // 
+            this.tabBar.Controls.Add(this.tabPage1);
+            this.tabBar.Controls.Add(this.optionsTab);
+            this.tabBar.Location = new System.Drawing.Point(12, 12);
+            this.tabBar.Name = "tabBar";
+            this.tabBar.SelectedIndex = 0;
+            this.tabBar.Size = new System.Drawing.Size(634, 426);
+            this.tabBar.TabIndex = 1;
+            // 
+            // optionsTab
+            // 
+            this.optionsTab.Controls.Add(this.flowLayoutPanel1);
+            this.optionsTab.Location = new System.Drawing.Point(4, 22);
+            this.optionsTab.Name = "optionsTab";
+            this.optionsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.optionsTab.Size = new System.Drawing.Size(626, 400);
+            this.optionsTab.TabIndex = 1;
+            this.optionsTab.Text = "Options";
+            this.optionsTab.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.channelLabel);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.bitMinimumUpDown);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.bpsUpDown);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.viewMinimumUpDown);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(7, 7);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(215, 231);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // channelLabel
+            // 
+            this.channelLabel.AutoSize = true;
+            this.channelLabel.Location = new System.Drawing.Point(3, 0);
+            this.channelLabel.Name = "channelLabel";
+            this.channelLabel.Size = new System.Drawing.Size(84, 13);
+            this.channelLabel.TabIndex = 0;
+            this.channelLabel.Text = "Twitch Channel:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bit Minimum:";
+            // 
+            // bitMinimumUpDown
+            // 
+            this.bitMinimumUpDown.Location = new System.Drawing.Point(3, 55);
+            this.bitMinimumUpDown.Name = "bitMinimumUpDown";
+            this.bitMinimumUpDown.Size = new System.Drawing.Size(120, 20);
+            this.bitMinimumUpDown.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bits Per Second:";
+            // 
+            // bpsUpDown
+            // 
+            this.bpsUpDown.Location = new System.Drawing.Point(3, 94);
+            this.bpsUpDown.Name = "bpsUpDown";
+            this.bpsUpDown.Size = new System.Drawing.Size(120, 20);
+            this.bpsUpDown.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "View Minimum:";
+            // 
+            // viewMinimumUpDown
+            // 
+            this.viewMinimumUpDown.Location = new System.Drawing.Point(3, 133);
+            this.viewMinimumUpDown.Name = "viewMinimumUpDown";
+            this.viewMinimumUpDown.Size = new System.Drawing.Size(120, 20);
+            this.viewMinimumUpDown.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(614, 364);
+            this.panel1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(626, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Video";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(614, 364);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Skip Video";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.tabBar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabBar.ResumeLayout(false);
+            this.optionsTab.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bitMinimumUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bpsUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewMinimumUpDown)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabBar;
+        private System.Windows.Forms.TabPage optionsTab;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label channelLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown bitMinimumUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown bpsUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown viewMinimumUpDown;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
